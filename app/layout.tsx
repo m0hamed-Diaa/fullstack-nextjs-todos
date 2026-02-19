cda3aab94599dcc63d4d872d3e0b6ed6cab2cf8c
Mohamed Diaa <mohameddiaaahdahed@gmail.com>
2026-02-20 01:11:38 +0200

    Add new features

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Todos controlling",
   description: "Create your todos now",
+  icons: "/colorful_todo_list.avif"
 };
 
 export default function RootLayout({
